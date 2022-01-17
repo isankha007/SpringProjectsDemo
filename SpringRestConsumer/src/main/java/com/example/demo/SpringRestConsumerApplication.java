9b8909c982dc5896fe2a003152fab214244c4489
isankha007 <sankhadeep2007@gmail.com>
2022-01-17 14:43:06 +0530

    files Added

--- SpringRestConsumer/src/main/java/com/example/demo/SpringRestConsumerApplication.java
+++ SpringRestConsumer/src/main/java/com/example/demo/SpringRestConsumerApplication.java
@@ -1,13 +1,37 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.restconsumer.Quote;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringRestConsumerApplication {
-
+	private static final Logger log=LoggerFactory.getLogger(SpringRestConsumerApplication.class) ;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestConsumerApplication.class, args);
+	}
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"https://quoters.apps.pcfone.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
 	}
 
 }

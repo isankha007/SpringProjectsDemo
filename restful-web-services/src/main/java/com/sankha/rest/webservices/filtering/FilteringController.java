44b865b636115be9e9547ecd35b398a7e4587898
isankha007 <sankhadeep2007@gmail.com>
2022-02-28 18:09:33 +0530

    dynamic filter added

--- restful-web-services/src/main/java/com/sankha/rest/webservices/filtering/FilteringController.java
+++ restful-web-services/src/main/java/com/sankha/rest/webservices/filtering/FilteringController.java
@@ -3,14 +3,26 @@ package com.sankha.rest.webservices.filtering;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
 	@GetMapping("/filtering")
-	public SomeBean retriveSomeBean() {
-		return new SomeBean("Value1","Value2","Value3");
+	public MappingJacksonValue retriveSomeBean() {
+		SomeBean someBean = new SomeBean("Value1","Value2","Value3");
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.
+				filterOutAllExcept("field1","field2");
+		FilterProvider filters=new SimpleFilterProvider().addFilter("Somefilter", filter);
+		
+		MappingJacksonValue mapping= new MappingJacksonValue(someBean);
+		mapping.setFilters(filters);
+		return mapping;
 	}
 	
 	@GetMapping("/filtering-list")
--- restful-web-services/src/main/java/com/sankha/rest/webservices/filtering/SomeBean.java
+++ restful-web-services/src/main/java/com/sankha/rest/webservices/filtering/SomeBean.java
@@ -1,9 +1,11 @@
 package com.sankha.rest.webservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(value = {"field1","field2"})
+//@JsonIgnoreProperties(value = {"field1","field2"})
+@JsonFilter("Somefilter")
 public class SomeBean {
 	private String field1;
 	private String field2;

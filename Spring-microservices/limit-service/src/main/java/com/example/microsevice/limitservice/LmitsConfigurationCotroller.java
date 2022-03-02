5ed49813089d86b25509e8bd3c3cb79e00ab42a4
isankha007 <sankhadeep2007@gmail.com>
2022-03-02 19:05:49 +0530

    Initial commit

---
+++ Spring-microservices/limit-service/src/main/java/com/example/microsevice/limitservice/LmitsConfigurationCotroller.java
@@ -0,0 +1,21 @@
+package com.example.microsevice.limitservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.microsevice.limitservice.bean.LimitConfiguration;
+
+@RestController
+public class LmitsConfigurationCotroller {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public LimitConfiguration retriveLimitsFromConfiguration() {
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+		
+	}
+
+}

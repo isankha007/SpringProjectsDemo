76efb16bca087734ac3d61e674cfd2fbefa88e22
isankha007 <sankhadeep2007@gmail.com>
2022-03-04 18:56:15 +0530

    Currency exchange added

--- Spring-microservices/limit-service/src/main/java/com/example/microsevice/limitservice/LmitsConfigurationCotroller.java
+++ Spring-microservices/limit-service/src/main/java/com/example/microsevice/limitservice/LmitsConfigurationCotroller.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.microsevice.limitservice.bean.LimitConfiguration;
+import com.example.microsevice.limitservice.confifuration.Configuration;
 
 @RestController
 public class LmitsConfigurationCotroller {

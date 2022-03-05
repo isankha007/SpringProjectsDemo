c8317219973171ddb4336a6be5d04a3e80c7c60c
isankha007 <sankhadeep2007@gmail.com>
2022-03-05 20:56:46 +0530

    currency conversion microservice files added

---
+++ Spring-microservices/currency-conversion-service/src/main/java/com/sankha/microservices/currencyconversionservice/CurrecnyConversion.java
@@ -0,0 +1,87 @@
+package com.sankha.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+public class CurrecnyConversion {
+	private Long id;
+	private String from;
+	private String to;
+	private BigDecimal qantity;
+	private BigDecimal conversionMultiple;
+	private BigDecimal totalCalculatedAmount;
+	private String environment;
+	
+	public CurrecnyConversion() {
+		
+	}
+	
+	public CurrecnyConversion(Long id, String from, String to, BigDecimal qantity,BigDecimal conversionMultiple,
+			BigDecimal totalCalculatedAmount, String environment) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+		this.qantity = qantity;
+		this.totalCalculatedAmount = totalCalculatedAmount;
+		this.environment = environment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
+
+	public BigDecimal getQantity() {
+		return qantity;
+	}
+
+	public void setQantity(BigDecimal qantity) {
+		this.qantity = qantity;
+	}
+
+	public BigDecimal getTotalCalculatedAmount() {
+		return totalCalculatedAmount;
+	}
+
+	public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
+		this.totalCalculatedAmount = totalCalculatedAmount;
+	}
+
+	public String getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+	
+
+}
---
+++ Spring-microservices/currency-conversion-service/src/main/java/com/sankha/microservices/currencyconversionservice/CurrwncyConversionController.java
@@ -0,0 +1,36 @@
+package com.sankha.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CurrwncyConversionController {
+	
+//	@Autowired
+//	private Environment environment;
+	
+	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrecnyConversion calculateCurrencyCOnversion(@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		Map<String,String> uriVariables=new HashMap();
+		uriVariables.put("from",from);
+		uriVariables.put("to",to);
+		ResponseEntity<CurrecnyConversion> responseEntity = new RestTemplate().getForEntity
+		("http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
+				CurrecnyConversion.class,uriVariables);
+		CurrecnyConversion currecnyConversion = responseEntity.getBody();
+		//String port =environment.getProperty("local.server.port");
+		BigDecimal conversionMultiple = currecnyConversion.getConversionMultiple();
+		return new CurrecnyConversion(currecnyConversion.getId(), from, to, quantity,conversionMultiple,
+				quantity.multiply(conversionMultiple), currecnyConversion.getEnvironment());
+		
+	}
+}

feff48970f0a5983b18dfc0c337295b1b29daacc
isankha007 <sankhadeep2007@gmail.com>
2022-03-26 11:43:53 +0530

    New gateway routing api project added

---
+++ Spring-microservices/api-gateway-1/src/main/java/com/sankha/microservices/currencyconversionservice/ApiGateway1Application.java
@@ -0,0 +1,13 @@
+package com.sankha.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateway1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateway1Application.class, args);
+	}
+
+}
